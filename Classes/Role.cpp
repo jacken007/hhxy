b8c1e17e62e340cfca244f5abb8292788248bfcf
jacken007 <64062157+jacken007@users.noreply.github.com>
2020-05-07 18:18:50 +0800

    Create Role.cpp

---
+++ Classes/Role.cpp
@@ -0,0 +1,19 @@
+﻿#include "Role.h"
+#include "string.h"
+#ifdef WIN32
+#pragma execution_character_set("utf-8")
+#endif
+
+bool CRole::init()
+{
+	//////////////////////////////
+	// 1. super init first
+	if (!CMapElement::init())
+	{
+		return false;
+	}
+
+    this->setElementName("再来者");
+    this->setStrModel("jianxiake");
+   	return true;
+}
